0cc0b078d2dd5e9e2549484eb3e21df40239a933
crisvlasova <vlasovacristina@gmail.com>
2022-07-13 16:46:12 -0300

    13-commit

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -7,7 +7,7 @@ export default function Experience () {
             <br/>
             <h5>My journey in programming started in January of 2022 when I met SoyHenry. After a few weeks
             learning about JavaScript and HTML I realeazed this was my dream job. I was so excited
-            that I started developing project by myself as the autodidact person I am; currently I have 2 projects:
+            that I started developing projects by myself as the autodidact person I am; currently I have 2 projects:
             one with only front end and the other one with front-end and back-end.
             Now I'm going through SoyHenry's last step: The final project, wich is about developing a SPA e-commerce,
             with this one (hopefully) I'm finishing this Full-Stack Developing course. <br/>
