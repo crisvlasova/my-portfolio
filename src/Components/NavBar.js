f044f5e388c9b55b1ff175ae9f8f7e4e926c5222
crisvlasova <vlasovacristina@gmail.com>
2022-07-14 17:26:24 -0300

    third-responsive

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,7 +5,7 @@ import menu from '../Images/icons8-menu-24.png';
 import '../StyleSheets/NavBar.css';
 
 export default function NavBar () {
-    const [active, setActive] = useState('')
+    const [active, setActive] = useState(false)
 
     let loc = useLocation().pathname
 
@@ -31,16 +31,16 @@ export default function NavBar () {
 
             <div className='button-container'>
                 <Link to='/'>
-                    <button value='/' className={loc === '/'? 'in-path navbar-button' : 'navbar-button'}>Home</button>
+                    <button className={loc === '/'? 'in-path navbar-button' : 'navbar-button'}>Home</button>
                 </Link>
                 <Link to='/aboutme'>
-                    <button value='/aboutme' className={loc === '/aboutme'? 'in-path navbar-button' : 'navbar-button'}>About me</button>
+                    <button className={loc === '/aboutme'? 'in-path navbar-button' : 'navbar-button'}>About me</button>
                 </Link>
                 <Link to='/experience'>
-                    <button value='/experience' className={loc === '/experience'? 'in-path navbar-button' : 'navbar-button'}>Experience</button>
+                    <button className={loc === '/experience'? 'in-path navbar-button' : 'navbar-button'}>Experience</button>
                 </Link>
                 <Link to='/projects'>
-                    <button value='/projects' className={loc === '/projects'? 'in-path navbar-button' : 'navbar-button'}>Projects</button>
+                    <button className={loc === '/projects'? 'in-path navbar-button' : 'navbar-button'}>Projects</button>
                 </Link>
                 {/* <button>EN</button>
                 <button>ES</button> */}
