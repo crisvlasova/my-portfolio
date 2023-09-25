58a8af702ab7f1c7d1af028accd9e1e8163bb404
crisvlasova <vlasovacristina@gmail.com>
2023-09-25 18:06:22 -0300

    new curriculum

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -41,7 +41,7 @@ export default function Home () {
                 <h1>Hi, I'm Cristina Vlassov</h1>
                 <h4>I'm a Junior Full-Stack Developer</h4>
 
-                <a href={downloadCv} download><button class='border border-0 rounded py-1 px-3'>Download my Curriculum Vitae </button></a>
+                <a href={downloadCv} onclick={window.open("../Images/Cristina Vlassov EN.pdf, _blank")}><button class='border border-0 rounded py-1 px-3'>See my curriculum</button></a>
                 {/* <img src={downloadLogo} alt='downloadlogo'/> */}
                 <div className='contact'>
                     {links.map(link => {return (
