29164a9ba6029c6c70c9560986e287210ae9ec12
crisvlasova <vlasovacristina@gmail.com>
2023-11-04 17:55:28 -0300

    particles changing

--- src/Components/Particle.js
+++ src/Components/Particle.js
@@ -118,7 +118,7 @@ export default function Particle () {
                     }
                 }
             },
-            "retina_detect": true,
+            "retina_detect": false,
         }}
         />
     )
