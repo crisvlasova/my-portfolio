99f6f4acdf919213ff0431ce6325ddec999bc4ec
crisvlasova <vlasovacristina@gmail.com>
2023-12-04 20:41:00 -0300

    asd

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import C from '../Images/icons8-constellation2-64.png'
+// import C from '../Images/icons8-constellation2-64.png'
 import { Link, useLocation } from 'react-router-dom';
 import menu from '../Images/icons8-menu-24.png';
 import '../StyleSheets/NavBar.css';
@@ -45,6 +45,15 @@ export default function NavBar() {
     return (
         <div className='navbar-menu-container'>
             <div className='d-flex flex-row justify-content-center align-items-center position-relative'>
+
+                {/* <nav className="navbar">
+                    <div className="container-fluid">
+                        <a className="navbar-brand">
+                            <img src={C} className="d-inline-block align-text-top" />
+                        </a>
+                    </div>
+                </nav> */}
+
                 <div className='responsive-menu'>
                     <button onClick={handleClick} className='menu-img bg-transparent border transparent border-0  text-white'>
                         <img src={menu}></img>
